5b9afb5828ce7f3e8b935221af253e946202dc50
Wondrow60 <130780440+Wondrow60@users.noreply.github.com>
2025-05-12 11:42:06 +0300

    main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Veri modeli (Email almak için)
 class Email(BaseModel):
     email: str
 
-# POST endpoint'i
 @app.post("/early-access")
 def collect_email(email: Email):
     print(f"Gelen e-posta: {email.email}")
