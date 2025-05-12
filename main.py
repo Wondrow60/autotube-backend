f5dd3339339f4c139a2e76e2ad03c46b1b737ebd
Wondrow60 <130780440+Wondrow60@users.noreply.github.com>
2025-05-12 12:25:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
-
-class Email(BaseModel):
-    email: str
-
-@app.post("/early-access")
-def collect_email(email: Email):
-    print(f"Gelen e-posta: {email.email}")
-    return {"message": "Kaydınız alındı!"}
