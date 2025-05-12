dbf6cb560fec091c10ea817084e8ac17967d26ef
Wondrow60 <130780440+Wondrow60@users.noreply.github.com>
2025-05-12 13:59:17 +0300

    Update and rename main.py to early-access.py

--- api/early-access.py
+++ api/early-access.py
@@ -6,7 +6,7 @@ app = FastAPI()
 class Email(BaseModel):
     email: str
 
-@app.post("/early-access")
+@app.post("/api/early-access")
 def collect_email(email: Email):
     print(f"Gelen e-posta: {email.email}")
     return {"message": "Kaydınız alındı!"}
